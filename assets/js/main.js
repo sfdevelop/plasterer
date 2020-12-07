4690cc64a6d88a8bb9e619460451fcb944281890
sfdevelop <sfdevelop747@gmail.com>
2020-12-07 14:33:32 +0200

    07-12

--- assets/js/main.js
+++ assets/js/main.js
@@ -19499,10 +19499,25 @@ $(document).ready(function () {
 	});
 });
 
+//якоря
 $("a.scroll-to").on("click", function (e) {
 	e.preventDefault();
 	var anchor = $(this).attr('href');
 	$('html, body').stop().animate({
 		scrollTop: $(anchor).offset().top
 	}, 800);
+});
+
+//боковое  меню
+$(document).ready(function () {
+	$("#navToggle").click(function () {
+		$(this).toggleClass("active");
+		$(".overlay").toggleClass("open");
+		// this line ▼ prevents content scroll-behind
+		$("body").toggleClass("locked");
+	});
+	$('.mobile_li').click(function (ev) {
+		$(this).find('>ul').slideToggle();
+		ev.stopPropagation();
+	});
 });
